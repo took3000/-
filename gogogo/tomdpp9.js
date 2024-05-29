d374170fe228b301725e642b68333f0c32b85db7
took3000 <54194865+took3000@users.noreply.github.com>
2024-05-29 18:48:26 +0800

    Update tomdpp9.js

--- gogogo/tomdpp9.js
+++ gogogo/tomdpp9.js
@@ -10,7 +10,7 @@ fetch('https://bizihu.com/nice/zuzutooga.txt', {
     quoteArray.forEach(quote => {
       const parts = quote.split(' | '); // 使用特定分隔符 " | " 将每行内容分割成部分
       const text = parts[0]; // 获取语录文本
-      const author = parts[3]; // 获取作者
+      const author = parts[1]; // 获取作者
       const date = parts[2];
       const div = document.createElement('div'); // 创建一个新的元素
       div.classList.add('quote'); // 为元素添加类名
@@ -18,12 +18,12 @@ fetch('https://bizihu.com/nice/zuzutooga.txt', {
       // 如果作者不为空
       if (author) {
         div.innerHTML = `
-         <a class="article" href="https://caiyun.139.com/m/i?105CfJsHH2CFC" target="_blank" >
+         <a class="article" href="${author || '#'}" target="_blank" >
               
-              <div class="date">${date}</div>
+              <div class="date">资源福利</div>
               <div class="title">${text}</div>
               <div class="desc">
-		      ${author || '点击跳转资源'}
+		      ${date || '暂无备注'}
               </div>
             </a>`; // 构建有作者信息的元素内容
       } else {
