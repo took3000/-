a19e13400692b0460f1ba7323c0bc0e52177a3ab
took3000 <54194865+took3000@users.noreply.github.com>
2024-05-29 18:37:05 +0800

    Update tomdpp9.js

--- gogogo/tomdpp9.js
+++ gogogo/tomdpp9.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
     // 使用 Fetch API 读取语录文件
-fetch('http://zi.bizihu.com/yulu/zuzutooga.txt', {
+fetch('http://zi.bizihu.com/zuzutooga.txt', {
       mode: 'no-cors'
     })
 .then(response => response.text())
