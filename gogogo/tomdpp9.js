b608a583e6996bf852632664d15917eed941a93a
took3000 <54194865+took3000@users.noreply.github.com>
2024-05-30 00:31:49 +0800

    Add files via upload

--- gogogo/tomdpp9.js
+++ gogogo/tomdpp9.js
@@ -1,42 +1,34 @@
 window.addEventListener('load', () => {
     // 使用 Fetch API 读取语录文件
-fetch('quotesWithAuthorsAndDates2.txt')
-.then(response => response.text())
-.then(quotes => {
-    const quoteArray = quotes.split('\n'); // 将语录文件内容按行分割成数组
+    fetch('./gogogo/quotesWithAuthorsAndDates2.txt')
+ .then(response => response.text())
+ .then(quotes => {
+        const parts = quotes.split('\n')[0].split(' | '); 
+        const text = parts[0]; 
+        const author = parts[2]; 
+        const link = parts[1];
+        const a = document.createElement('a'); // 创建新元素为 a
+        a.classList.add('article'); // 为 a 添加样式类 "article"
+        a.target = "_blank"; // 设置 a 的属性 target="_blank"
+        a.href = `${link}`; // 设置 a 的链接为 ${link}
 
-    quoteArray.forEach(quote => {
-      const parts = quote.split(' | '); // 使用特定分隔符 " | " 将每行内容分割成部分
-      const text = parts[0]; // 获取语录文本
-      const author = parts[2]; // 获取作者
-      const date = parts[1];
-      const div = document.createElement('div'); // 创建一个新的元素
-      div.classList.add('quote'); // 为元素添加类名
+        // 如果作者不为空
+        if (author) {
+            a.innerHTML = `
+                  <div class="date">资源福利</div>
+                  <div class="title">${text}</div>
+                  <div class="desc">
+                  ${author}
+                  </div>
+                `; 
+        } else {
+            a.innerHTML = `
+                  <div class="date">资源福利</div>
+                  <div class="title">${text}</div>
+                `; 
+        }
 
-      // 如果作者不为空
-      if (author) {
-        div.innerHTML = `
-         <a class="article" href="${date || '#'}" target="_blank" >
-              
-              <div class="date">资源福利</div>
-              <div class="title">${text}</div>
-              <div class="desc">
-		      ${author}
-              </div>
-            </a>`; // 构建有作者信息的元素内容
-      } else {
-        div.innerHTML = `
-         <a class="article" href="${date || '#'}" target="_blank" >
-              
-              <div class="date">资源福利</div>
-              <div class="title">${text}</div>
-              
-            </a>`; // 构建没有作者信息时的元素内容
-      }
-
-      document.getElementById('quotes').appendChild(div); // 将元素添加到指定容器
-    });
-  })
-.catch(error => console.error('读取语录文件时发生错误:', error)); // 处理错误情况
-
+        document.getElementById('quotes').appendChild(a); 
+    })
+.catch(error => console.error('读取语录文件时发生错误:', error)); 
 });
